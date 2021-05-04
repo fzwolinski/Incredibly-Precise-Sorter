0d283fc3f173c60837958bc7eaa2bd0ff8112a78
Fabian Zwolinski <Giovacho@tuta.io>
2021-05-04 23:46:27 +0200

    Move declarations from header to cpp file

--- sorting_alg.hpp
+++ sorting_alg.hpp
@@ -2,35 +2,5 @@
 
 class SortingAlg {
 public:
-  
-  static std::vector<int> bubble_sort(std::vector<int> const& data) {
-    /* 
-    * Sorts an array with Bubble Sort algorithm
-    * 
-    * :param data: data (int numbers) to be sorted
-    * :return: sorted data as vector<int>
-    */
-    const auto numbers_count = data.size();
-    auto sorted = data;
-
-    for (int i = 0; i < numbers_count - 1; i++) {
-      auto swapped = false;
-      for (int j = 0; j < numbers_count - i - 1; j++) {
-        if (sorted[j] > sorted[j+1]) {
-          // Swap places
-          std::swap(sorted[j], sorted[j+1]);
-          swapped = true;
-        }
-      }
-      if (swapped == false) {
-          // Array sorted
-          break;
-      }
-    }
-    
-    return sorted;
-  }
-
-
-
+  static std::vector<int> bubble_sort(std::vector<int> const& data);
 };
